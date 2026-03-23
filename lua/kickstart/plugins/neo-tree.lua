25a7e8a517880efdaf273b83fd40e35badbfb695
ari <piumpium@paranoici.org>
2026-03-23 23:15:52 +0100

    feat: updated neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,15 +11,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>te', ':Neotree toggle<CR>', desc = '[T]oggle [E]xplorer', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  opts = {},
 }
